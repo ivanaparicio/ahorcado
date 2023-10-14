ec0b4c5d2be9b7edc4a372fb91863357f22c9a4c
Ivan Aparicio <n.ivan.aparicio@gmail.com>
2023-10-14 13:09:44 -0500

    fix se cambio el nombre de la carpeta de compilacion

---
+++ vite.config.js
@@ -0,0 +1,10 @@
+// vite.config.js
+
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  // Otras configuraciones...
+  build: {
+    outDir: 'docs', // Cambia esto a la ubicación deseada
+  },
+});
